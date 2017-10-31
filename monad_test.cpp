8ccec448d5d8263f73a89356558ebc6d02a4d0ca
XzoRit <xzorit@hotmail.com>
2017-10-31 11:33:10 +0100

    implement lazy value

--- monad_test.cpp
+++ monad_test.cpp
@@ -1,5 +1,34 @@
 #include <boost/test/unit_test.hpp>
-#include <vector>
+#include <functional>
+#include <mutex>
+#include <optional>
+#include <utility>
+
+namespace monad
+{
+    using namespace std;
+
+    template <class A>
+    class val
+    {
+    private:
+        A m_func;
+        mutable optional<decltype(m_func())> m_value;
+        mutable mutex m_lock_value;
+    public:
+        val(const A& func)
+            : m_func{func}
+            , m_value{}
+            {}
+        auto operator()() const
+            {
+                lock_guard<mutex> _(m_lock_value);
+                if(!m_value) m_value = invoke(m_func);
+
+                return *m_value;
+            }
+    };
+}
 
 struct init
 {
@@ -15,9 +44,13 @@ struct init
 
 BOOST_AUTO_TEST_SUITE(vector)
 
-BOOST_FIXTURE_TEST_CASE(size_and_capacity_after_init, init)
+BOOST_FIXTURE_TEST_CASE(monad_val, init)
 {
-    BOOST_TEST(true == true);
+    using namespace monad;
+    const auto a = val([](){ return 2374; });
+
+    BOOST_TEST(a() == 2374);
+    BOOST_TEST(a() == 2374);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
