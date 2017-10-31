c7316dc38da059697f1d8652f42142ea5156c963
XzoRit <xzorit@hotmail.com>
2017-10-31 15:18:55 +0100

    put monad code into its own header file

--- monad_test.cpp
+++ monad_test.cpp
@@ -1,11 +1,7 @@
+#include "monad.hpp"
 #include <boost/test/unit_test.hpp>
-#include <functional>
-#include <mutex>
-#include <optional>
-#include <utility>
-#include <variant>
-#include <iostream>
 #include <string>
+#include <iostream>
 
 namespace std
 {
@@ -13,53 +9,6 @@ namespace std
     {
         ostr << "monostate";
         return ostr;
-    }
-}
-
-using namespace std;
-
-namespace monad
-{
-    template <class A>
-    class lazy_value
-    {
-    private:
-        A m_func;
-        mutable optional<decltype(m_func())> m_value;
-        mutable mutex m_lock_value;
-    public:
-        constexpr lazy_value(const A& func)
-            : m_func{func}
-            , m_value{}
-            {}
-        constexpr auto operator()() const
-            {
-                lock_guard<mutex> _(m_lock_value);
-                if(!m_value) m_value = invoke(m_func);
-
-                return *m_value;
-            }
-    };
-
-    template<class A, class B>
-    class io
-    {
-    public:
-        constexpr io(const B& action)
-            : m_action{action}
-            {}
-        constexpr A operator()() const
-            {
-                return invoke(m_action);
-            }
-    private:
-        B m_action;
-    };
-
-    template<class A>
-    constexpr auto make_io_action(A&& action)
-    {
-        return io<decltype(action()), A>{forward<A>(action)};
     }
 }
 
@@ -75,6 +24,7 @@ struct init
     }
 };
 
+using namespace std;
 using namespace monad;
 
 const auto putCout = [](const auto& s)
