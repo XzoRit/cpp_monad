ae2e83e0fb0749f5d78cb55d31dfe2cbb7f90eb4
XzoRit <xzorit@hotmail.com>
2017-11-05 10:14:25 +0100

    use functions for bind/fmap in main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include "monad.hpp"
 #include <iostream>
+#include <string>
+#include <cctype>
+#include <variant>
 
 using namespace std;
 using namespace monad;
@@ -10,22 +13,41 @@ const auto putCout = [](const auto& s)
         [s](){ cout << s; return monostate{}; });
 };
 
-const auto getCout = [](const auto&)
+auto getCout(const monostate&)
 {
     return make_io_action(
-        [](){ string s{}; getline(cin, s); return s; });
-};
+        []()
+        {
+            string s{};
+            getline(cin, s);
+            return s;
+        });
+}
+
+string str_tolower(string a)
+{
+    transform(
+        a.begin(), a.end(),
+        a.begin(),
+        [](unsigned char c)
+        { return tolower(c); }
+        );
+    return a;
+}
 
 auto test()
 {
-    return putCout("your name please: ")
-        | getCout
+    return
+        (putCout("your name please: ") | getCout)
+        > str_tolower
+        > [](const auto& a){ return string{"\""} + a + '\"'; }
         | [](const auto& a){ return putCout(string{"Hello "} + a + "!\n"); }
         ;
 }
 
 int main()
 {
-    test()();
+    const auto a{test()};
+    a();
     return 0;
 }
